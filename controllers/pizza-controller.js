592e5ea6767f411552e55330faff62567e7b93a6
Lyle Kilson <lylekilsongithub@gmail.com>
2021-05-29 16:33:55 -0600

    issue #1 completed

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,75 @@
+const { Pizza } = require("../models");
+const { db } = require("../models/Pizza");
+
+const pizzaController = {
+  // get all pizzas
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // get one pizza by id
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then((dbPizzaData) => {
+        // if no pizza is found, send 404
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // create a pizza
+  // With this createPizza() method, we destructure the body out of the Express.js req object because we don't need to interface with any of the other data it provides.
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // update pizza by id
+  // with this .findOneAndUpdate() method, Mongoose finds a single document we want to update, then updates it and returns the updated document
+  // if we don't set that third parameter, { new: true }, it will return the original document
+  // By setting the parameter to true, we're instructing Mongoose to return the new version of the document
+
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // delete a pizza
+  // In this example, we use the Mongoose .findOneAndDelete() method, which will find the document to be returned and also delete it from the database
+  // Like with updating, we could alternatively use .deleteOne() or .deleteMany(), but we're using the .findOneAndDelete() method because it provides a little more data in case the client wants it.
+
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+// for more details visit https://mongoosejs.com/docs/queries.html
+
+module.exports = pizzaController;

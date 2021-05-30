17d4f84a063ef205a985c5533838bdc686033e6f
Lyle Kilson <lylekilsongithub@gmail.com>
2021-05-30 14:25:09 -0600

    Issue #3 completed

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,12 +2,17 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 // /api/comments/<pizzaId>
 router.route("/:pizzaId").post(addComment);
 
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
 // /api/comments/<pizzaId>/<commentId>
-router.route("/:pizzaId/:commentId").delete(removeComment);
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
 
 module.exports = router;
